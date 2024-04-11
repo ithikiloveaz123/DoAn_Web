11b0a79b522436323ececff241660554eab4a0eb
ADMIN <ADMIN@AKELO>
2024-04-11 09:15:42 +0700

    update

---
+++ Controllers/WTN.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DoAn_Web.Models;
+
+namespace DoAn_Web.Controllers
+{
+    public class WTN : Controller
+    {
+        DoAnWebContext db = new DoAnWebContext();
+        public IActionResult Index()
+        {
+            var listphong = db.Phongs.ToList();
+            return View(listphong);
+        }
+    }
+}
